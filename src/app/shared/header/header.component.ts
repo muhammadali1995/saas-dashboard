7260aa48d89ce76091ad27e02a8d4371560f2d65
Diyorbekabdumannonov <94838816+Diyorbekabdumannonov@users.noreply.github.com>
2024-09-25 10:58:34 +0500

    updates

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { DropdownComponent } from '../dropdown/dropdown.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [DropdownComponent, FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  options = ['Option 1', 'Option 2', 'Option 3'];
+  faNotification = faBell;
 }
