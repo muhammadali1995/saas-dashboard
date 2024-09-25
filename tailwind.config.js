a27167cdb50c9472a8f5f9d7a1200e9534ca5e96
Diyorbekabdumannonov <94838816+Diyorbekabdumannonov@users.noreply.github.com>
2024-09-25 09:01:11 +0500

    sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f5f7fa",
+        secondary: "#335cff"
+      }
+    },
   },
   plugins: [],
 }
